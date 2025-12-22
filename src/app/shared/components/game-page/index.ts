87c46205da56eb3cbf233dd6ab7c6c2cc9efedef
Denys <denys.meniashev@symphony-solutions.com>
2025-12-22 20:15:27 +0200

    redundance clean up

--- src/app/shared/components/game-page/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './game-page.component';
-export * from './game-page.module';
-export * from './fullscreen-confirm-dialog/fullscreen-confirm-dialog.component';
-export * from './exit-confirm-dialog/exit-confirm-dialog.component';
-
